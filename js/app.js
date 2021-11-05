38d14b2826f9b62914323db0bfcfa8ba90416af5
Pedro G. Perez <pedrogperezalvarez@gmail.com>
2021-11-04 22:56:55 -0700

    Final version lab 12

--- js/app.js
+++ js/app.js
@@ -10,9 +10,9 @@ const results = document.querySelector('ul');
 
 let allProducts = [];
 let clicks = 0;
-let noRepeats = [0,0,0]
-let deDupe = []
-const clicksAllowed = 20;
+let noRepeats = [];
+let productSet = [];
+const clicksAllowed = 15;
 
 function Product(name, fileExtension = 'jpg') {
   this.name = name;
@@ -47,31 +47,29 @@ function selectRandomProduct() {
 }
 
 function renderProduct() {
-  let Product1 = selectRandomProduct();
-  let Product2 = selectRandomProduct();
-  let Product3 = selectRandomProduct();
-  let noRepeats = [Product1,Product2,Product3]
-  let deDupe = new Set(noRepeats);
 
-  while (noRepeats.length != deDupe.size) {
-      Product1 = selectRandomProduct();
-      Product2 = selectRandomProduct();
-      Product3 = selectRandomProduct(); 
-      noRepeats = [Product1,Product2,Product3]
-      deDupe = new Set(noRepeats);
+  while (productSet.length < 6) {
+    let ranNum = selectRandomProduct();
+    if (!productSet.includes(ranNum)) {
+      productSet.push(ranNum);
+    }
   }
 
-    image1.src = allProducts[Product1].src;
-    image1.alt = allProducts[Product1].name;
-    allProducts[Product1].views++;
-    image2.src = allProducts[Product2].src;
-    image2.alt = allProducts[Product2].name;
-    allProducts[Product2].views++;   
-    image3.src = allProducts[Product3].src;
-    image3.alt = allProducts[Product3].name;
-    allProducts[Product3].views++;
-}
+  let product1 = productSet.shift();
+  let product2 = productSet.shift();
+  let product3 = productSet.shift();
 
+  image1.src = allProducts[product1].src;
+  image1.alt = allProducts[product1].name;
+  allProducts[product1].views++;
+  image2.src = allProducts[product2].src;
+  image2.alt = allProducts[product2].name;
+  allProducts[product2].views++;   
+  image3.src = allProducts[product3].src;
+  image3.alt = allProducts[product3].name;
+  allProducts[product3].views++;
+
+  }
 
 function handleProductClick(event) {
   if (event.target === myContainer) {
@@ -89,17 +87,60 @@ function handleProductClick(event) {
   renderProduct();
   if (clicks === clicksAllowed) {
     myContainer.removeEventListener('click', handleProductClick);
-    myButton.addEventListener('click', handleButtonClick);
-    myButton.className = 'clicks-allowed';
+    renderChart();
   }
 }
 
-function handleButtonClick() {
+function renderChart() {
+  let productNames = [];
+  let productLikes = [];
+  let productViews = [];
   for (let i = 0; i < allProducts.length; i++) {
-    let li = document.createElement('li')
-    li.textContent = `${allProducts[i].name} had ${allProducts[i].views} view and was clicked ${allProducts[i].likes} times.`;
-    results.appendChild(li);
+    productNames.push(allProducts[i].name);
+    productLikes.push(allProducts[i].likes);
+    productViews.push(allProducts[i].views);
+  }
+  // console.log(ProductLikes);
+
+  const data = {
+    labels: productNames,
+    datasets: [{
+      label: 'Likes',
+      data: productLikes,
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.8)'
+      ],
+      borderColor: [
+        'rgb(255, 99, 132)'
+      ],
+      borderWidth: 1
+    },
+    {
+      label: 'Views',
+      data: productViews,
+      backgroundColor: [
+        'rgba(255, 159, 64, 0.8)'
+      ],
+      borderColor: [
+        'rgb(255, 159, 64)'
+      ],
+      borderWidth: 1
+    }]
   };
+
+  const config = {
+    type: 'bar',
+    data: data,
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    },
+  };
+  let canvasChart = document.getElementById('myChart');
+  const myChart = new Chart(canvasChart,config);
 }
 
 renderProduct();
